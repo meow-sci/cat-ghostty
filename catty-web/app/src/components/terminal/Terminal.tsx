84ebb80514bf008fcb59e3ca1a258320fa7aae7b
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2025-12-15 08:58:14 -0500

    some rudimentary terminal page layout

---
+++ catty-web/app/src/components/terminal/Terminal.tsx
@@ -0,0 +1,8 @@
+export function Terminal() {
+  return (
+    <>
+      <div id="terminal"></div>
+      <input id="input" type="text" />
+    </>
+  )
+}
---
+++ catty-web/app/src/components/terminal/TerminalPage.tsx
@@ -0,0 +1,9 @@
+import { Terminal } from "./Terminal";
+
+export function TerminalPage() {
+  return (
+    <main id="page">
+      <Terminal />
+    </main>
+  );
+}
