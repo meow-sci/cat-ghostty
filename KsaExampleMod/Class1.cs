05ed42f8c10358f06b602398b1238e929d8b6bfe
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2025-12-21 12:16:12 -0500

    a very simple example dotnet project of a ksa game mod

---
+++ KsaExampleMod/Class1.cs
@@ -0,0 +1,69 @@
+﻿using Brutal.ImGuiApi;
+using StarMap.API;
+using KSA;
+using System.Diagnostics.CodeAnalysis;
+using HarmonyLib;
+using System.Reflection;
+using Microsoft.VisualBasic;
+
+namespace modone
+{
+    public class SimpleMod : IStarMapMod, IStarMapOnUi
+    {
+        public bool ImmediateUnload => false;
+
+        public void OnAfterUi(double dt)
+        {
+            // The original flags assignement - just to show some options
+            // ImGuiWindowFlags flags = ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.MenuBar;
+
+            ImGuiWindowFlags flags = ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoBackground | ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.MenuBar;
+
+            Vehicle? vehicle = Program.ControlledVehicle;
+
+            if (vehicle != null)
+                {
+                ImGui.Begin("MyWindow", flags);
+
+                if (ImGui.BeginMenuBar())
+                {
+                    if (ImGui.BeginMenu("Modone - Stats"))
+                    {
+                        if (ImGui.MenuItem("Show Message 2"))
+                        {
+                            Console.WriteLine("Hello from modone!");
+                        }
+                        ImGui.EndMenu();
+                    }
+                    ImGui.EndMenuBar();
+                }
+                ImGui.Text($"Current Propellant Mass: {vehicle.FlightComputer.PropellantMass:F3}");
+                ImGui.Text($"Total Mass: {vehicle.FlightComputer.TotalMass}");
+                ImGui.Text($"Total Acceleration: {vehicle.FlightComputer.TotalAcceleration}");
+                ImGui.Text($"Current simulation Speed: {Universe.SimulationSpeed.ToString()}");
+                ImGui.Text($"Time: {Universe.GetElapsedSimTime().Days} days");
+
+                ImGui.End();
+            }
+        }
+
+        public void OnBeforeUi(double dt)
+        {
+            
+        }
+
+        public void OnFullyLoaded()
+        {
+            Patcher.Patch();
+        }
+
+        public void OnImmediatLoad()
+        {
+        }
+
+        public void Unload()
+        {
+            Patcher.Unload();
+        }
+    }
+}
---
+++ KsaExampleMod/Patcher.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace modone
+{
+    [HarmonyPatch]
+    internal static class Patcher
+    {
+        private static Harmony? _harmony = new Harmony("StarMap.SimpleMod");
+
+        public static void Patch()
+        {
+            Console.WriteLine("Patching SimpleMod...");
+            _harmony?.PatchAll();
+        }
+
+        public static void Unload()
+        {
+            _harmony?.UnpatchAll(_harmony.Id);
+            _harmony = null;
+        }
+    }
+}
