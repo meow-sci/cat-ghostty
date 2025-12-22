594734cbe877a4393e31e17cfd7d1967157ac046
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2025-12-22 09:25:08 -0500

    some initial debug refactors for the standalone gui test app

--- KsaImGuiTestWin/StandaloneImGui.cs
+++ KsaImGuiTestWin/StandaloneImGui.cs
@@ -12,7 +12,7 @@ public static class StandaloneImGui
 {
   private static GlfwWindow? window;
   private static Renderer? renderer;
-  private static RenderPassState? rstate;
+  private static RenderPassState rstate = default!;
   private static Action? OnDrawUi;
 
   public static void Run(Action onDrawUi)
@@ -20,8 +20,10 @@ public static class StandaloneImGui
     OnDrawUi = onDrawUi;
     Init();
 
-    while (!window!.ShouldClose){
-      OnFrame();}
+    while (!window!.ShouldClose)
+    {
+      OnFrame();
+    }
   }
 
   private static void Init()
@@ -29,32 +31,25 @@ public static class StandaloneImGui
     Glfw.Init();
 
     Glfw.WindowHint(GlfwWindowHint.ClientApi, 0);
-    Glfw.WindowHint(GlfwWindowHint.AutoIconify, 0);
-    Glfw.WindowHint(GlfwWindowHint.FocusOnShow, 1);
     window = Glfw.CreateWindow(new()
     {
       Title = "ImGui Test",
-      Size = new int2(1200, 800),
+      Size = new int2(1500, 1500),
     });
 
-    renderer = new Renderer(window, VkFormat.D32SFloat, VkPresentModeKHR.FifoKHR, VulkanHelpers.Api.VERSION_1_3);
+    renderer = new Renderer(window, VkFormat.Undefined, VkPresentModeKHR.FifoKHR, VulkanHelpers.Api.VERSION_1_3);
 
     rstate = new RenderPassState
     {
-      Pass = renderer.MainRenderPass,
-      SampleCount = VkSampleCountFlags._1Bit,
-      ClearValues = [
-        new VkClearColorValue() { Float32 = Color.Black.AsFloat4 },
-          new VkClearDepthStencilValue() { Depth = 0 },
-        ]
+      ClearValues = [new VkClearColorValue() { Float32 = Color.Black.AsFloat4 }]
     };
 
-    ImGui.CreateContext();
-    var io = ImGui.GetIO();
-    io.ConfigDpiScaleFonts = true;
-    io.ConfigDpiScaleViewports = true;
 
+    ImGui.CreateContext();
     ImGuiBackend.Initialize(window, renderer);
+
+    var io = ImGui.GetIO();
+    io.IniSavingRate = 0f; // disable imgui.ini state saving
 
     // This requires the working directory to be set to the KSA install (or the cwd having a Content folder with at least one ttf)
     KSA.Program.ConsoleWindow = new(); // so fontmanager doesn't throw
@@ -66,46 +61,37 @@ public static class StandaloneImGui
     Glfw.PollEvents();
     ImGuiBackend.NewFrame();
     ImGui.NewFrame();
-    ImGuiHelper.StartFrame();
 
+    // call custom ui code
     OnDrawUi!();
 
     ImGui.Render();
+
     var (result, frame) = renderer!.TryAcquireNextFrame();
     if (result != FrameResult.Success)
     {
-      RebuildRenderer();
-      (result, frame) = renderer.TryAcquireNextFrame();
+      renderer.Rebuild(VkPresentModeKHR.FifoKHR);
+      renderer.Device.WaitIdle();
+      return;
     }
-    if (result != FrameResult.Success)
-      throw new InvalidOperationException($"{result}");
 
     var (resources, commandBuffer) = frame;
-    var begin = new VkRenderPassBeginInfo()
+
+    commandBuffer.Begin(VkCommandBufferUsageFlags.OneTimeSubmitBit);
+    commandBuffer.BeginRenderPass(new VkRenderPassBeginInfo()
     {
       RenderPass = renderer.MainRenderPass,
       Framebuffer = resources.Framebuffer,
       RenderArea = new(renderer.Extent),
-      ClearValues = rstate!.ClearValues.Ptr,
-      ClearValueCount = 2,
-    };
+      ClearValues = rstate.ClearValues.Ptr,
+      ClearValueCount = 1,
+    }, VkSubpassContents.Inline);
 
-    commandBuffer.Reset();
-    commandBuffer.Begin(VkCommandBufferUsageFlags.OneTimeSubmitBit);
-    commandBuffer.BeginRenderPass(in begin, VkSubpassContents.Inline);
     ImGuiBackend.Vulkan.RenderDrawData(commandBuffer);
+
     commandBuffer.EndRenderPass();
     commandBuffer.End();
 
-    var frameResult = renderer.TrySubmitFrame();
-    if (frameResult != FrameResult.Success)
-      RebuildRenderer();
-  }
-
-  private static void RebuildRenderer()
-  {
-    renderer!.Rebuild(VkPresentModeKHR.FifoKHR);
-    renderer.Device.WaitIdle();
-    rstate!.Pass = renderer.MainRenderPass;
+    renderer.TrySubmitFrame();
   }
 }
